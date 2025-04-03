ab24743dc57c2301a3988cf49dff2d753b4521a3
Caitlin O'Hara <csoh20@bath.ac.uk>
2025-04-03 11:10:47 +0100

    Thomas Algorithm for batching

--- src/peds_model/T_Alg.py
+++ src/peds_model/T_Alg.py
@@ -2,20 +2,22 @@
 import torch 
 DTYPE = torch.float64 
 def thomas_algorithm(a, b, c, f):
-    N = b.shape[0]
+    B, N = b.shape[0]
     b, f = b.clone(), f.clone()
+
+    u = torch.zeros_like(f)
     
     # Forward elimination
     for i in range(1, N):
-        w = a[i-1] / b[i-1]
-        b[i] -= w * c[i-1]
-        f[i] -= w * f[i-1]
+        w = a[:, i - 1] / b[:, i - 1]
+        b[:, i] = b[:, i] - w * c[:, i - 1]
+        f[:, i] = f[:, i] - w * f[:, i - 1]
+
 
     # Back substitution
-    u = torch.zeros(N, dtype=DTYPE)
-    u[-1] = f[-1] / b[-1]
-    for i in range(N-2, -1, -1):
-        u[i] = (f[i] - c[i] * u[i+1]) / b[i]
+    u[:, -1] = f[:, -1] / b[:, -1]
+    for i in reversed(range(N - 1)):
+        u[:, i] = (f[:, i] - c[:, i] * u[:, i + 1]) / b[:, i]
 
     return u
 
