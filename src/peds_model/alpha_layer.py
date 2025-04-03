8e25adeb5eae4879f4a9c2fb356a37583e4270d5
Caitlin O'Hara <csoh20@bath.ac.uk>
2025-04-03 10:51:38 +0100

    changed alpha_layer forward pass for batching

--- src/peds_model/alpha_layer.py
+++ src/peds_model/alpha_layer.py
@@ -15,8 +15,15 @@ class AlphaFunction(torch.autograd.Function):
     def forward(ctx, metadata, input):
         """
         Forward solve: u = A(alpha)^(-1) f
+        change shape to (B,N) for batches 
         """
         f = metadata["f"].to(DTYPE)
+
+        # Make sure f is right shape 
+        if f.ndim == 1:
+            f = f.unsqueeze(0).expand(input.shape[0], -1)
+
+
         a, b, c = AlphaFunction.tridiagonal_matrix(input)
         u = thomas_algorithm(a, b, c, f)
         ctx.save_for_backward(input, a, b, c, u, f)
@@ -31,10 +38,16 @@ class AlphaFunction(torch.autograd.Function):
         c_j = alpha_{j+1}^2 + 2*alpha_{j+1}
         """
         alpha = alpha.to(DTYPE)
-        N = alpha.shape[0]
-        a = alpha[:-1] ** 2
+
+        # Make sure alpha is right shape
+        if alpha.ndim == 1:
+            alpha = alpha.unsqueeze(0)
+
+
+        #N = alpha.shape[0]
+        a = alpha[:, :-1] ** 2 # All rows, drops last column (B, N-1)
         b = 1 + alpha ** 3
-        c = alpha[1:] ** 2 + 2 * alpha[1:]
+        c = alpha[:, 1:] ** 2 + 2 * alpha[:, 1:] # drops first column (B, N-1)
         return a, b, c
 
     @staticmethod
@@ -43,23 +56,24 @@ class AlphaFunction(torch.autograd.Function):
         Adjoint gradient: grad_input[j] = - w^T (dA/dalpha_j * u)
         where A^T w = grad_output, solved via Thomas algorithm.
         """
-        input, a, b, c, u, f = ctx.saved_tensors
-        N = input.shape[0]
-        w = thomas_algorithm(c, b, a, grad_output.to(DTYPE))
-        grad_input = torch.zeros_like(input, dtype=DTYPE)
+        raise NotImplementedError("Backward pass not implemented yet.")
+        #input, a, b, c, u, f = ctx.saved_tensors
+        #N = input.shape[0]
+        #w = thomas_algorithm(c, b, a, grad_output.to(DTYPE))
+        #grad_input = torch.zeros_like(input, dtype=DTYPE)
 
-        for j in range(N):
-            dA_dalpha_u_j = torch.zeros(N, dtype=DTYPE)
+        #for j in range(N):
+            #dA_dalpha_u_j = torch.zeros(N, dtype=DTYPE)
 
-            if j < N - 1:
-                dA_dalpha_u_j[j + 1] += 2 * input[j] * u[j]
-            dA_dalpha_u_j[j] += 3 * input[j]**2 * u[j]
-            if j > 0:
-                dA_dalpha_u_j[j - 1] += (2 * input[j] + 2) * u[j]
+            #if j < N - 1:
+                #dA_dalpha_u_j[j + 1] += 2 * input[j] * u[j]
+            #dA_dalpha_u_j[j] += 3 * input[j]**2 * u[j]
+            #if j > 0:
+                #dA_dalpha_u_j[j - 1] += (2 * input[j] + 2) * u[j]
 
-            grad_input[j] = - torch.dot(w, dA_dalpha_u_j)
+            #grad_input[j] = - torch.dot(w, dA_dalpha_u_j)
 
-        return None, grad_input
+        #return None, grad_input
     
 class AlphaLayer(torch.nn.Module):
     def __init__(self, f):
