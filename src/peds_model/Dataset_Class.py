fe8fcd4016e0e10e2e097008fab9cb1a54bb483e
Caitlin O'Hara <csoh20@bath.ac.uk>
2025-05-13 10:53:14 +0100

    Dataset_Class - added comments

--- src/peds_model/Dataset_Class.py
+++ src/peds_model/Dataset_Class.py
@@ -1,34 +1,36 @@
 import torch
 from torch.utils.data import Dataset
-from peds_model.Alpha_Dataset import AlphaFieldDataset
-from peds_model.alpha_layer import AlphaLayer
+from peds_model.Alpha_Dataset import AlphaFieldDataset #generate alpha fields
+from peds_model.alpha_layer import AlphaLayer # solver 
 from peds_model.QoI import QuantityOfInterest
 
 class AlphaQoIDataset(Dataset):
     def __init__(self, n_samples=100, L=1000, qoi_points=None, **alpha_kwargs):
         """
         Dataset returning (alpha, QoI(u)) pairs.
-
-        Args:
-            n_samples (int): Number of samples.
-            L (int): Spatial resolution.
-            qoi_points (list): Sample points for QoI (e.g. [0.25, 0.5, 0.75])
-            alpha_kwargs: Passed to AlphaFieldDataset (e.g., n_fibres, sigma)
         """
-        self.alpha_dataset = AlphaFieldDataset(n_samples=n_samples, L=L, **alpha_kwargs)
-        self.x = self.alpha_dataset.x
+        self.alpha_dataset = AlphaFieldDataset(n_samples=n_samples, L=L, **alpha_kwargs) # creates alpha dataset
+        self.x = self.alpha_dataset.x # gets spatial grid used to generate alpha 
         self.qoi = QuantityOfInterest(qoi_points or [0.25, 0.5, 0.75], x_grid=self.x)
         self.model = AlphaLayer(f=torch.ones(L, dtype=torch.float64))
 
-    def __len__(self):
+    def __len__(self): # gives number of samples 
         return len(self.alpha_dataset)
 
+    
     def __getitem__(self, idx):
-        alpha, _ = self.alpha_dataset[idx]  # [L]
-        alpha_batched = alpha.unsqueeze(0)  # [1, L]
-        u = self.model(alpha_batched).squeeze(0)  # [L]
-        qoi_u = self.qoi(u)  # [m]
-        return alpha, qoi_u
+        alpha, _ = self.alpha_dataset[idx]
+        alpha_batched = alpha.unsqueeze(0)
+        u = self.model(alpha_batched).squeeze(0)
+        qoi_u = self.qoi(u)
+
+        alpha_tensor = alpha.to(torch.float32)
+        qoi_tensor = torch.as_tensor(qoi_u, dtype=torch.float32)  # <- this should be a flat 1D tensor
+       
+
+        return alpha_tensor, qoi_tensor
+
+
 
 
 # Create the dataset
