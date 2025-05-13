dffadf1ef1cd2a298b88da93bcc59e1de8eca26c
Caitlin O'Hara <csoh20@bath.ac.uk>
2025-05-13 10:50:49 +0100

    Quantity of Interest - Pytorch Module

--- src/peds_model/QoI.py
+++ src/peds_model/QoI.py
@@ -1,17 +1,19 @@
 import torch
 
-class QuantityOfInterest:
+class QuantityOfInterest(torch.nn.Module):
     def __init__(self, sample_points, x_grid):
-        self.sample_points = sample_points
-        self.x_grid = torch.tensor(x_grid, dtype=torch.float64)
+
+        super().__init__()
+        self.sample_points = sample_points # where we want to sample 
+        self.x_grid = torch.tensor(x_grid, dtype=torch.float64) # converts spatial grid into pytorch tensor 
         self.indices = self._compute_indices()
 
     def _compute_indices(self):
         indices = []
         for xi in self.sample_points:
-            idx = (torch.abs(self.x_grid - xi)).argmin().item()
+            idx = (torch.abs(self.x_grid - xi)).argmin().item() # finds index of smallest distane (grid point closest)
             indices.append(idx)
         return indices
 
-    def __call__(self, u):
+    def forward(self, u):
         return u[self.indices]
