bc38bd44d661b1a24a7b24a793d3d43e7d0681e7
Caitlin O'Hara <csoh20@bath.ac.uk>
2025-06-21 11:25:10 +0100

    main hybrid - altered

--- src/main_hybrid.py
+++ src/main_hybrid.py
@@ -1,47 +1,9 @@
-#import torch
-#import torch.nn as nn
 
-#from peds_model.Downsample_Class import DownsampleAlpha
-#from training_NN import AlphaNN
-#from peds_model.alpha_layer import AlphaLayer
-#from peds_model.QoI import QuantityOfInterest
-
-#class HybridAlphaModel(nn.Module):
-    #def __init__(self, input_length=1000, downsample_factor=2, qoi_points=[0.25, 0.5, 0.75]):
-        #super().__init__()
-
-        # Input
-        #self.downsampler = DownsampleAlpha(factor=downsample_factor)
-        #self.alpha_nn = AlphaNN(input_length=input_length, output_length=(input_length // downsample_factor + 1))
-
-        # PDE solver
-        #self.alpha_layer = AlphaLayer(f=torch.ones(input_length // downsample_factor + 1, dtype=torch.float32))
-
-        # Quantity of Interest extractor
-        #x_grid_downsampled = torch.linspace(0, 1, input_length // downsample_factor + 1)
-        #self.qoi_extractor = QuantityOfInterest(qoi_points, x_grid=x_grid_downsampled)
-
-    #def forward(self, alpha_highres):
-        #if alpha_highres.ndim == 1:
-            #alpha_highres = alpha_highres.unsqueeze(0)  # Ensure batch dim
-
-        # Compute downsampled and NN-based alpha **independently**
-        #alpha_ds = self.downsampler(alpha_highres)       # [B, L_ds]
-        #alpha_nn = self.alpha_nn(alpha_highres)          # [B, L_ds]
-
-        # Combine to get corrected alpha (alpha tilde)
-        #alpha_tilde = 0.5 * (alpha_ds + alpha_nn)        # [B, L_ds]
-
-        # Solve PDE and extract QoI
-        #u_pred = self.alpha_layer(alpha_tilde)           # [B, L_ds]
-        #qoi = torch.stack([self.qoi_extractor(u_i) for u_i in u_pred])  # [B, num_qoi]
-
-        #return qoi
 import torch
 import torch.nn as nn
 
 from peds_model.Downsample_Class import DownsampleAlpha
-from training_NN import AlphaNN
+from training_alpha_NN import AlphaNN
 from peds_model.alpha_layer import AlphaLayer
 from peds_model.QoI import QuantityOfInterest
 
